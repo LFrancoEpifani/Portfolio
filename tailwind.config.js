b2b6269fb8fe5207ce39cef023dbda7d4eec006b
LucianoEsplai <lfrancoepifani@gmail.com>
2023-12-28 11:54:47 +0100

    Starting new portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+    // Add other paths here if you have more file types or different paths
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
